07d7ed6dc221f9fb45510b77e43c4f465256c43f
Gianluca Tuscano <tusca95@gmail.com>
2021-11-17 18:28:04 +0100

    Update Ps1.tsx

--- src/components/ps1/Ps1.tsx
+++ src/components/ps1/Ps1.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 export const Ps1 = () => {
   return (
     <div>
-      <span className="text-light-yellow dark:text-dark-yellow">guest</span>
+      <span className="text-light-yellow dark:text-dark-yellow">randomuser</span>
       <span className="text-light-gray dark:text-dark-gray">@</span>
-      <span className="text-light-green dark:text-dark-green">m4tt72.com</span>
+      <span className="text-light-green dark:text-dark-green">gianlucatuscano.it</span>
       <span className="text-light-gray dark:text-dark-gray">:$ ~ </span>
     </div>
   );
