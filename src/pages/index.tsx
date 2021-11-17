17ffe4256de4ca50390ad0c0cdf37a3a0c0f630a
Gianluca Tuscano <tusca95@gmail.com>
2021-11-17 18:24:02 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ const IndexPage: React.FC<{ version: string; quote: string }> = ({
   return (
     <>
       <Head>
-        <title>M4TT72 | Home</title>
+        <title>sintomaticonegativo | gianlucatuscano</title>
       </Head>
 
       <div className="p-8 overflow-hidden h-full border-2 rounded border-light-yellow dark:border-dark-yellow">
