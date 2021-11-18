1933712dccaf2ae3aa876399ce1888199791115e
Gianluca Tuscano <tusca95@gmail.com>
2021-11-18 17:50:28 +0100

    Update weather.ts

--- src/utils/bin/weather.ts
+++ src/utils/bin/weather.ts
@@ -4,7 +4,7 @@ const weather = async (args: string[]): Promise<string> => {
   const city = args.join('+');
 
   if (!city) {
-    return 'Usage: weather [city]. Example: weather casablanca';
+    return 'Usage: weather [city]. Example: weather San Donà di Piave';
   }
 
   const weather = await getWeather(city);
